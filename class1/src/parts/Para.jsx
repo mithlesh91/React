c4e0a0ad10d3b2f1c12586d535c5e27c4bec6bb7
Mithlesh <aryanrajnwd9155@gmail.com>
2025-12-28 23:07:15 +0530

    box incre Decre

--- class1/src/parts/Para.jsx
+++ class1/src/parts/Para.jsx
@@ -1,31 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
 const Para = () => {
   const [count, setCount] = useState(0)
 
-// //  let count = 0;
-//   const btnclick= () =>{
-//     // count++;
-//     // console.log(count);
-//     setCount(count + 1);
-//   }
-
   return (
-    <div>
-      <h1>{count}</h1>
-      <button className='bg-blue-700 h-7 w-25 rounded-2xl'
-      onClick={() =>
-       setCount(count + 1)}
-       >Incresase</button>
-        <button className='bg-blue-700 h-7 w-25 rounded-2xl'
-      onClick={() =>
-       setCount(count - 1)}
-       >Decrease</button>
-        <button className='bg-blue-700 h-7 w-25 rounded-2xl'
-      onClick={() =>
-       setCount(count + 5)}
-       >Jump by</button>
-       
+    <div className='gap-10'> 
+      <div className="bg-blue-800 w-200 h-200 p-20 m-10">
+      <h1 className="text-white text-9xl">{count}</h1>
+      </div>
+      <button className='bg-blue-500 w-100 h-50' onClick={()=>
+        setCount(count + 1)
+      }>Increase</button>
+
+      <button className='bg-red-500 w-100 h-50' onClick={()=>
+        setCount(count - 1)
+      }>Decrease</button>
+      
     </div>
   )
 }
