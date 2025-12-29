a2e903cd11cfdf7a1c13093e1d075d200c60d426
Mithlesh <aryanrajnwd9155@gmail.com>
2025-12-29 14:02:26 +0530

    get grace

--- class1/src/parts/Para.jsx
+++ class1/src/parts/Para.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
+// const [Marks,setMarks]=useState([40,50,60,70,80])
+// const Marks=[40,50,60,70,80 ];
+
 
 const Para = () => {
-  const [count, setCount] = useState(0)
-
+  const [Marks,setMarks]=useState([40,50,60,70,80])
+  function handleClick(){
+    const newMarks=Marks.map(function(elem){
+      return elem +5
+    })
+    setMarks(newMarks)
+  }
+  
   return (
-    <div className='gap-10'> 
-      <div className="bg-blue-800 w-200 h-200 p-20 m-10">
-      <h1 className="text-white text-9xl">{count}</h1>
-      </div>
-      <button className='bg-blue-500 w-100 h-50' onClick={()=>
-        setCount(count + 1)
-      }>Increase</button>
-
-      <button className='bg-red-500 w-100 h-50' onClick={()=>
-        setCount(count - 1)
-      }>Decrease</button>
-      
+    <div>
+      {Marks.map(function(elem,index){
+        return <h1 key={index}>student {index+1} ={elem}</h1>
+      })}
+      <button onClick={handleClick} className='bg-orange-300 h-7 w-20'>click me</button>
     </div>
   )
 }
