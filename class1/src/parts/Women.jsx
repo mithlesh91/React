79afdb756f00003b79ebd48fa9e080e7dcf086f9
Mithlesh <aryanrajnwd9155@gmail.com>
2025-12-29 14:41:46 +0530

    click male female button

---
+++ class1/src/parts/Women.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Women = () => {
+  return (
+    <div className='bg-pink-500 h-10 w-20 flex items-center justify-center rounded-b-md'>
+      <h1>Women</h1>
+    </div>
+  )
+}
+
+export default Women
